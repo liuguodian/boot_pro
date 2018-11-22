89e335faa6b11191065addd1d99ce3ecc101337e
dian <956461675@qq.com>
2018-11-22 17:46:00 +0800

    创建restful_service服务

---
+++ src/main/java/com/demo/boot_pro/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.demo.boot_pro.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2018/11/22 0022.
+ */
+/*@CrossOrigin  //跨域 */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+}
