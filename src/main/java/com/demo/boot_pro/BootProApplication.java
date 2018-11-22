06c904caeea905cdb5b8f1e983f49537b7e06f2e
dian <956461675@qq.com>
2018-11-22 17:39:31 +0800

    初始提交

---
+++ src/main/java/com/demo/boot_pro/BootProApplication.java
@@ -0,0 +1,12 @@
+package com.demo.boot_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProApplication.class, args);
+	}
+}
