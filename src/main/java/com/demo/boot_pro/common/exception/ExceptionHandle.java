35cb11a7b92a0cb990056bf8168b97a35009d1b7
dian <956461675@qq.com>
2018-11-23 16:54:54 +0800

    增加日志输出和打包程序

--- src/main/java/com/demo/boot_pro/common/exception/ExceptionHandle.java
+++ src/main/java/com/demo/boot_pro/common/exception/ExceptionHandle.java
@@ -2,8 +2,6 @@ package com.demo.boot_pro.common.exception;
 
 import com.demo.boot_pro.common.exception.errorcode.CommonErrorCode;
 import com.demo.boot_pro.common.vo.ResponseResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +18,6 @@ import javax.xml.transform.Result;
 @ControllerAdvice
 public class ExceptionHandle {
 
-    private final static Logger logger = LoggerFactory.getLogger(ExceptionHandle.class);
 
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
